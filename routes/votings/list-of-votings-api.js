68bb87c845b256d0eb5d39894e1457665beb4716
Tomas Peterec <info@tomaspeterec.sk>
2023-12-19 18:32:56 +0100

    userId header catching implemented for get and post

--- routes/votings/list-of-votings-api.js
+++ routes/votings/list-of-votings-api.js
@@ -1,64 +1,85 @@
 const Joi = require('joi');
 const cors = require('cors');
 const express = require('express');
-const db = require("./list-of-votings-f");
+const db = require('./list-of-votings-f');
 
 const router = express.Router();
 const reactJsRootUrl = process.env.ALLOWED_ORIGINS;
+
+router.use((req, res, next) => {
+  // Access the 'X-User-ID' header from the request
+  const userId = req.headers['x-user-id'];
+
+  // Store the User ID in req.locals for later use
+  req.locals = {
+    userId: userId,
+  };
+
+  next();
+});
 
 router.use(cors({
   origin: reactJsRootUrl ? [reactJsRootUrl] : [], // Use the origin directly
 }));
 router.use(express.json());
 
-//get all Lists 
+// Get all lists 
 router.get("/", async (req, res) => {
   const allLists = await db.getAllLists();
   res.send(allLists);
 });
 
-//get part of Lists by user id (id_of_user)
-router.get("/:userId", async (req, res) => {
-  console.log("zachyt get")
-  const partOfLists = await db.getUserLists(req.params.userId);
+// Get part of lists by user ID (id_of_user)
+router.get("/subset", async (req, res) => {
+  // Access the User ID from req.locals
+  const partOfLists = await db.getUserLists(req.locals.userId);
   res.send(partOfLists);
 });
 
-//adds a new user vote
+// Add a new user vote
 router.post("/", async (req, res) => {
-  console.log("zachyt post")
-  const {error} = validateUser(req.body)
-  if(error) return res.status(400).send(error.details[0].message)
-  const recordedList = await db.createList(req.body)
-  res.send(recordedList)
+  try {
+    const userVoteData = {
+      ...req.body,
+      id_of_user: req.locals.userId,
+    };
+    const { error } = validateUser(userVoteData);
+    if (error) return res.status(400).send(error.details[0].message);
+    const recordedList = await db.createList(userVoteData);
+    res.send(recordedList);
+  } catch (error) {
+    console.error('Error creating user vote:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  try{
-    const updatedList = await db.updateList(req.params.id, req.body)
-    if(updatedList) {
+  try {
+    const updatedList = await db.updateList(req.params.id, req.body);
+    if (updatedList) {
       return res.send('The userList with the given ID was updated.');
     } else {
       return res.status(404).send('The userList with the given ID was not found.');
     }
-  }catch (error) {
-    return res.status(500).send(error.message);
+  } catch (error) {
+    console.error('Error updating user vote:', error);
+    res.status(500).send('Internal Server Error');
   }
-})
+});
 
 router.delete('/:id', async (req, res) => {
-  try{
-    const userList = await db.deleteList(req.params.id)
-    if(userList) {
+  try {
+    const userList = await db.deleteList(req.params.id);
+    if (userList) {
       return res.send('The userList with the given ID was deleted.');
     } else {
       return res.status(404).send('The userList with the given ID was not found.');
     }
-  }catch (error) {
-    return res.status(500).send(error.message);
+  } catch (error) {
+    console.error('Error deleting user vote:', error);
+    res.status(500).send('Internal Server Error');
   }
-
-})
+});
 
 function validateUser(userListName) {
   const schema = {
